d879f4e0447f509dc02cb00b7c6255d2b5ad2a8c
powpilot <unitedthings@yahoo.com>
2018-06-04 13:25:31 +0300

    enabed the buttons on bus and logs pages

---
+++ bus.vue
@@ -0,0 +1,27 @@
+<template>
+    
+  
+    <div>
+        <div class="title text-xs-center py-2"></div>
+         <bus-table></bus-table>
+    </div>
+</template>
+
+<script>
+	import Vue from "vue";
+	import Axios from "axios";
+	import VueAxios from "vue-axios";
+	Vue.use(VueAxios, Axios);
+
+	import bus_table from "./bus_table.vue";
+	Vue.component('bus-table', bus_table)
+
+
+	export default {
+		data: () => ({
+
+		}),
+
+	};
+
+</script>
