2cd26f7f4fd37f1b77f4ef18dae87f8ab2b19cd1
vvzvlad <vvzvlad@gmail.com>
2018-06-06 14:52:03 +0300

    fix indenting

--- src/file_editors/files_create_button.vue
+++ src/file_editors/files_create_button.vue
@@ -1,26 +1,21 @@
 <template>
-<div>
-
-<v-layout row >
-     
-    <v-flex xs-6 ml-3>
-        <v-text-field light
-          id="testing"
-          name="input-1"
-          label="file name"
-          v-model= "filenameinput"
-        ></v-text-field>
-    </v-flex>
-    <v-flex xs-6 mt-2>
-         <v-btn color="success" left :path="path" :filetype="filetype" @click="create_file(path,filetype)">
-            <v-icon  left small>fa-file</v-icon>Create new file
-         </v-btn>
-         
-    </v-flex>
-      
- </v-layout>   
-
-</div>
+   <div>
+      <v-layout row >
+         <v-flex xs-6 ml-3>
+            <v-text-field light
+               id="testing"
+               name="input-1"
+               label="file name"
+               v-model= "filenameinput"
+            ></v-text-field>
+         </v-flex>
+         <v-flex xs-6 mt-2>
+               <v-btn color="success" left :path="path" :filetype="filetype" @click="create_file(path,filetype)">
+                  <v-icon left small>fa-file</v-icon> Create new file
+               </v-btn>
+         </v-flex>
+      </v-layout>
+   </div>
 </template>
 
 <script>
@@ -31,22 +26,20 @@ Vue.use(VueAxios, Axios);
 
 export default {
   data: () => ({
-  filenameinput : "",
-    }),
-  props: ["path","filetype"],
+    filenameinput: ""
+  }),
+  props: ["path", "filetype"],
 
   methods: {
-    create_file(path,filetype) {
-       Vue.axios
-        .get("http://localhost:8080/system_webedit_data",{
+    create_file(path, filetype) {
+      Vue.axios
+        .get("http://localhost:8080/system_webedit_data", {
           params: {
             item: "new",
-            address: path + '/'+this.filenameinput + "."+ filetype
-                     }
+            address: path + "/" + this.filenameinput + "." + filetype
+          }
         })
-        .then(response => {
- 
-        })
+        .then(response => {})
         .catch(error => {
           console.log(error);
         });
@@ -54,4 +47,3 @@ export default {
   }
 };
 </script>
-    
