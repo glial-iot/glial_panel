1e58b01fe2db4756ca6db5823e4b0da6df20d3d0
vvzvlad <vvzvlad@gmail.com>
2018-06-08 20:09:14 +0300

    fix components snackbar bugs

---
+++ src/snackbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-snackbar :timeout="10000" :top="true" :right="true"  v-model="visible"  :color='color' >
+       {{text}}
+       <v-btn flat  @click.native="visible = false">Close</v-btn>
+  </v-snackbar>
+</template>
+<script>
+import Vue from "vue";
+export default {
+  data: () => ({
+    visible: false,
+    text: "",
+    color: ""
+  }),
+  methods: {
+    update(message, color) {
+      this.visible = false;
+      if (message !== "") {
+        this.text = message;
+        this.visible = true;
+        this.color = color || "error";
+      }
+    }
+  }
+};
+</script>
+
+
