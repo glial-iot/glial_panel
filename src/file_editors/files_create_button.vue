f7e3ac28780eb589c3cd35c332e3ed4d720b6f36
powpilot <unitedthings@yahoo.com>
2018-06-08 17:56:50 +0300

    snackbar as a vue-component
    added the new component - gluebar.vue
    switched files_create_button.vue from using a classic snackbar to gluebar for displaying errors

--- src/file_editors/files_create_button.vue
+++ src/file_editors/files_create_button.vue
@@ -3,8 +3,9 @@
       <v-layout row >
          <v-flex xs-6 ml-3>
             <v-text-field light
+               id="testing"
                name="input-1"
-               label="filename"
+               label="file name"
                v-model= "filenameinput"
             ></v-text-field>
          </v-flex>
@@ -14,6 +15,7 @@
                </v-btn>
          </v-flex>
       </v-layout>
+      <gluebar ref="gluebar"></gluebar>
    </div>
 </template>
 
@@ -22,48 +24,53 @@ import Vue from "vue";
 import Axios from "axios";
 import VueAxios from "vue-axios";
 Vue.use(VueAxios, Axios);
+import gluebar from "./gluebar.vue";
+Vue.component("gluebar", gluebar);
 
 export default {
   data() {
     return {
-      button_create: {
+    button_create: {
         disabled: false,
         loading: false,
         color: "primary"
       },
-      filenameinput: ""
-    };
+    filenameinput: ""
+  }
   },
   props: ["path", "filetype"],
 
   methods: {
     create_file(path, filetype) {
       Vue.axios
-        .get("http://localhost:8080/system_webedit_data_v2", {
+        .get("http://localhost:8080/system_webedit_data", {
           params: {
             item: "new",
             address: path + "/" + this.filenameinput + "." + filetype
           }
         })
         .then(response => {
-          setTimeout(() => (this.button_create.loading = false), 800);
-          setTimeout(() => (this.button_create.disabled = false), 800);
-          this.button_create.color = "success";
-          this.button_create.loading = true;
-          this.button_create.disabled = true;
-          setTimeout(() => (this.button_create.color = "primary"), 1500);
-          this.$emit("data_updated", path);
-          this.filenameinput = "";
+      
+            setTimeout(() => (this.button_create.loading = false), 800);
+            setTimeout(() => (this.button_create.disabled = false), 800);
+            this.button_create.color = "success";
+            this.button_create.loading = true;
+            this.button_create.disabled = true;
+            setTimeout(() => (this.button_create.color = "primary"), 1500);
+            this.$emit("data_updated", path);
+            this.filenameinput = "";
+
         })
         .catch(error => {
-          console.log(error);
-          setTimeout(() => (this.button_create.loading = false), 800);
-          setTimeout(() => (this.button_create.disabled = false), 800);
-          this.button_create.color = "error";
-          this.button_create.loading = true;
-          this.button_create.disabled = true;
-          setTimeout(() => (this.button_create.color = "primary"), 2000);
-          this.$emit("create_error");
+              
+            setTimeout(() => (this.button_create.loading = false), 800);
+            setTimeout(() => (this.button_create.disabled = false), 800);
+            this.button_create.color = "error";
+            this.button_create.loading = true;
+            this.button_create.disabled = true;
+            setTimeout(() => (this.button_create.color = "primary"), 2000);
+            this.$refs.gluebar.display('ERROR CREATING NEW FILE');
+        
         });
     }
   }
---
+++ src/file_editors/gluebar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-snackbar :timeout="5000" :top="true" :left="true"  v-model="snackbar"  color="red" >
+       {{snackbartext}}
+       <v-btn flat  @click.native="snackbar = false">Close</v-btn>
+  </v-snackbar> 
+</template>
+<script>
+import Vue from "vue";
+export default {
+  data: () => ({
+    snackbar: false,
+    snackbartext: ""
+  }),
+  methods: {
+      display(message){
+          this.snackbartext = message;
+          this.snackbar = true;
+           }
+         }
+};
+</script>
+
+
