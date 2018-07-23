d2597ea5df5ec8a02c7a66910d30bb78fd4fd208
vvzvlad <vvzvlad@gmail.com>
2018-07-23 17:31:20 +0300

    update controls

--- src/controls.vue
+++ src/controls.vue
@@ -3,13 +3,13 @@
       <div class="title text-xs-center py-1">Controls</div>
 
       <div class="text-xs-center">
-         <v-btn :loading="button_wipe.loading" :disabled="button_wipe.disabled" :color="button_wipe.color" @click.native="button = 'button_wipe'">
+         <v-btn color="secondary" @click.native="send('wipe_storage')">
             <v-icon left small>fa-exclamation-triangle</v-icon> Wipe storage and stop
          </v-btn>
-         <v-btn :loading="button_stop.loading" :disabled="button_stop.disabled" :color="button_stop.color" @click.native="button = 'button_stop'">
+         <v-btn color="secondary" @click.native="send('tarantool_stop')">
             <v-icon left small>fa-stop-circle</v-icon> Tarantool stop
          </v-btn>
-         <v-btn :loading="button_update.loading" :disabled="button_update.disabled" :color="button_update.color" @click.native="button = 'button_update'">
+         <v-btn color="secondary" @click.native="send('update')">
             <v-icon left small>fa-cloud-download-alt</v-icon> GLUE update and stop
          </v-btn>
       </div>
@@ -30,70 +30,26 @@ Vue.component("snackbar", snackbar);
 
 export default {
   data() {
-    return {
-      button: null,
-      color: null,
-      button_wipe: {
-        disabled: false,
-        loading: false,
-        color: "secondary",
-        action: "wipe_storage"
-      },
-      button_stop: {
-        disabled: false,
-        loading: false,
-        color: "secondary",
-        action: "tarantool_stop"
-      },
-      button_update: {
-        disabled: false,
-        loading: false,
-        color: "secondary",
-        action: "update"
-      },
-      button_edit: {
-        disabled: false,
-        loading: false,
-        color: "secondary",
-        action: ""
-      }
-    };
+    return {};
   },
 
-  watch: {
-    button() {
-      if (this.button != undefined) {
-        const l = this.button;
-        this[l].loading = true;
-        //this[l].disabled = true;
-        Vue.axios
-          .get(this.$store.getters.server_url + "/system_event", {
-            params: {
-              action: this[l].action
-            }
-          })
-          .then(response => {
-            let old_color = this[l].color;
-            setTimeout(() => (this[l].loading = false), 800);
-            setTimeout(() => (this[l].disabled = false), 800);
-            this[l].color = "success";
-            setTimeout(() => (this[l].color = old_color), 1500);
-            console.log(response);
-            console.log(response.data.msg);
-            this.$refs.snackbar.update(response.data.msg, "success", 5000);
-          })
-          .catch(error => {
-            let old_color = this[l].color;
-            setTimeout(() => (this[l].loading = false), 800);
-            setTimeout(() => (this[l].disabled = false), 800);
-            this[l].color = "error";
-            setTimeout(() => (this[l].color = old_color), 2000);
-            console.log(error);
-            this.$refs.snackbar.update("Network error");
-          });
-
-        this.button = null;
-      }
+  methods: {
+    send(action) {
+      console.log(action);
+      Vue.axios
+        .get(this.$store.getters.server_url + "/system_event", {
+          params: {
+            action: action
+          }
+        })
+        .then(response => {
+          console.log(response.data.msg);
+          this.$refs.snackbar.update(response.data.msg, "success", 5000);
+        })
+        .catch(error => {
+          console.log(error);
+          this.$refs.snackbar.update("Network error");
+        });
     }
   }
 };
