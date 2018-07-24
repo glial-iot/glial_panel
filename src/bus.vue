d5461b32b3dcd85d9824360ace9091298ac597e9
Nail Ismangulov <n.ismangulov@gmail.com>
2018-07-24 11:19:45 +0600

    move buttons to external components

--- src/bus.vue
+++ src/bus.vue
@@ -43,27 +43,9 @@
                      <div class="ellipsis" :title="props.item.timestamp">{{ props.item.timestamp }}</div>
                   </td>
                   <td class="justify-center layout px-0 button-sm">
-                     <button type="button" class="mx-0 v-btn v-btn--icon" @click="topic_delete(props.item)">
-                       <div class="v-btn__content">
-                         <svg aria-hidden="true" class="svg-inline--fa fa-trash-alt fa-w-14 v-icon pink--text" style="font-size: 16px;" data-prefix="fa" data-icon="trash-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg="">
-                          <path fill="currentColor" d="M0 84V56c0-13.3 10.7-24 24-24h112l9.4-18.7c4-8.2 12.3-13.3 21.4-13.3h114.3c9.1 0 17.4 5.1 21.5 13.3L312 32h112c13.3 0 24 10.7 24 24v28c0 6.6-5.4 12-12 12H12C5.4 96 0 90.6 0 84zm416 56v324c0 26.5-21.5 48-48 48H80c-26.5 0-48-21.5-48-48V140c0-6.6 5.4-12 12-12h360c6.6 0 12 5.4 12 12zm-272 68c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208zm96 0c0-8.8-7.2-16-16-16s-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208z"></path>
-                         </svg>
-                       </div>
-                     </button>
-                     <button type="button" class="ml-0 mr-2 v-btn v-btn--icon" v-show="props.item.tsdb_save" @click="tsdb_set(props.item)">
-                       <div class="v-btn__content">
-                         <svg aria-hidden="true" class="svg-inline--fa fa-download fa-w-16 v-icon green--text" style="font-size: 16px;" data-prefix="fa" data-icon="download" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
-                           <path fill="currentColor" d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path>
-                         </svg>
-                       </div>
-                     </button>
-                     <button type="button" class="ml-0 mr-2 v-btn v-btn--icon" v-show="!props.item.tsdb_save" @click="tsdb_set(props.item)">
-                       <div class="v-btn__content">
-                         <svg aria-hidden="true" class="svg-inline--fa fa-download fa-w-16 v-icon grey--text" style="font-size: 16px;" data-prefix="fa" data-icon="download" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
-                           <path fill="currentColor" d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path>
-                         </svg>
-                       </div>
-                     </button>
+                     <button-trash @click="topic_delete(props.item)"></button-trash>
+                     <button-download v-show="props.item.tsdb_save" @click="tsdb_set(props.item)"></button-download>
+                     <button-download-disabled v-show="!props.item.tsdb_save" @click="tsdb_set(props.item)"></button-download-disabled>
                   </td>
                </tr>
             </template>
@@ -89,9 +71,17 @@ import VueAxios from "vue-axios";
 Vue.use(VueAxios, Axios);
 
 import snackbar from "./components/snackbar.vue";
-Vue.component("snackbar", snackbar);
+import buttonTrash from "./components/buttons/button-trash.vue";
+import buttonDownload from "./components/buttons/button-download.vue";
+import buttonDownloadDisabled from "./components/buttons/button-download-disabled.vue";
 
 export default {
+  components: {
+    snackbar,
+    buttonTrash,
+    buttonDownload,
+    buttonDownloadDisabled
+  },
   data: () => ({
     update_interval: "2000",
     bus_values: [],
