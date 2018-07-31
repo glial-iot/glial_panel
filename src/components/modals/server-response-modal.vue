db6f20b700a5d504ac2fd81e77c61c6d7b798937
Nail Ismangulov <n.ismangulov@gmail.com>
2018-07-31 09:45:47 +0600

    format string with nl2br

--- src/components/modals/server-response-modal.vue
+++ src/components/modals/server-response-modal.vue
@@ -2,7 +2,7 @@
    <v-dialog :value="visible" persistent max-width="290">
       <v-card>
          <v-card-title class="headline">Server Response</v-card-title>
-         <v-card-text><span v-html="nl2br(message)"></span></v-card-text>
+         <v-card-text><span v-html="format_text(message)"></span></v-card-text>
          <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="green darken-1" flat @click="hide()">Close</v-btn>
@@ -13,6 +13,8 @@
 
 
 <script>
+import {nl2br} from "../../utils/index.js";
+
 export default {
   data: () => ({
     visible: false,
@@ -26,13 +28,8 @@ export default {
     hide() {
       this.visible = false;
     },
-    nl2br(str, is_xhtml) {
-      if (typeof str === 'undefined' || str === null) {
-        return '';
-      }
-      
-      var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
-      return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+    format_text(text) {
+      return nl2br(text)
     }
   }
 };
