0c85e613c44f32ecf3ed216d553a830d0fec6eed
vvzvlad <vvzvlad@gmail.com>
2018-08-09 20:05:22 +0300

    add type to human-readable human string

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,3 +6,22 @@ export function nl2br(str, is_xhtml) {
    var breakTag = is_xhtml || typeof is_xhtml === "undefined" ? "<br />" : "<br>";
    return (str + "").replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, "$1" + breakTag + "$2");
 }
+
+
+export function script_type2string(type) {
+   if (type === "WEB_EVENT") {
+      return "Web-event script";
+   }
+   if (type === "BUS_EVENT") {
+      return "Bus-event script";
+   }
+   if (type === "DRIVER") {
+      return "Driver script";
+   }
+   if (type === "TIMER_EVENT") {
+      return "Timer-event script";
+   }
+   if (type === "SHEDULE_EVENT") {
+      return "Shedule-event script";
+   }
+}
