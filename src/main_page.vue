5c6cb803baee4342ac8a5292b0db6fee79e86873
Nikolay Matyukhin <matuhin86@gmail.com>
2018-09-03 14:07:38 +0300

    Revert "Server address to lowercase"
    This reverts commit 2e7ea5ed7f5c2573b5d86253ac91a067e151ef53.

--- src/main_page.vue
+++ src/main_page.vue
@@ -43,7 +43,7 @@
             <v-flex xs2 class="flex-end">
                <v-menu offset-y>
                   <v-btn slot="activator" color="primary" depressed dark class="select-server">
-                     <p class="server-address">{{ `${server_scheme}://${server_address}:${server_port}` }}</p>
+                     <p>{{ `${server_scheme}://${server_address}:${server_port}` }}</p>
                      <div class="icon-wrapper" v-show="server_online">
                         <v-icon title="Server online" color="green" small>fa-link</v-icon>
                      </div>
@@ -230,10 +230,6 @@ table.v-table thead tr {
   left: -4px !important;
   top: -4px !important;
   border-radius: 0;
-}
-
-.v-btn .server-address {
-   text-transform:none;
 }
 
 .v-navigation-drawer {
