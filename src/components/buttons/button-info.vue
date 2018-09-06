38831f5ec5fd3de846d6edfbf51a551b27c09abf
vvzvlad <vvzvlad@gmail.com>
2018-09-06 16:32:05 +0300

    remove old no working styles fix

--- src/components/buttons/button-info.vue
+++ src/components/buttons/button-info.vue
@@ -1,27 +1,12 @@
 <template>
-   <button type="button" :class="get_class()">
+   <button type="button" class="btn-icon v-btn v-btn--icon">
       <div class="v-btn__content">
-         <svg aria-hidden="true" class="svg-inline--fa fa-info fa-w-6 v-icon blue--text" style="font-size: 16px;" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512">
+         <svg aria-hidden="true" class="svg-inline--fa fa-info fa-w-6 v-icon v-icon blue--text" style="font-size: 16px;" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512">
             <path fill="currentColor" d="M20 424.229h20V279.771H20c-11.046 0-20-8.954-20-20V212c0-11.046 8.954-20 20-20h112c11.046 0 20 8.954 20 20v212.229h20c11.046 0 20 8.954 20 20V492c0 11.046-8.954 20-20 20H20c-11.046 0-20-8.954-20-20v-47.771c0-11.046 8.954-20 20-20zM96 0C56.235 0 24 32.235 24 72s32.235 72 72 72 72-32.235 72-72S135.764 0 96 0z"></path>
          </svg>
       </div>
    </button>
 </template>
-
-<script>
-export default {
-  props: ["item"],
-  methods: {
-    get_class: function() {
-      if (this.item && this.item.index === 0) {
-        return "btn-icon v-btn v-btn--icon fix-first";
-      }
-
-      return "btn-icon v-btn v-btn--icon";
-    }
-  }
-};
-</script>
 
 <style scoped>
 .v-btn--icon {
@@ -30,14 +15,10 @@ export default {
 }
 
 .btn-icon:before {
-  height: 24px !important;
-  width: 24px !important;
+  height: 25px !important;
+  width: 25px !important;
   left: 0px !important;
   top: 0px !important;
   border-radius: 0;
-}
-
-.btn-icon.fix-first:before {
-  height: 25px !important;
 }
 </style>
