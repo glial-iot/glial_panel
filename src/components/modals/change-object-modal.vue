db94eda1f44f9e7b0e4122cf201ca4fa7759f313
Nikolay Matyukhin <matuhin86@gmail.com>
2018-10-12 14:53:29 +0300

    Added shortcuts for most used object values in script details modal.

--- src/components/modals/change-object-modal.vue
+++ src/components/modals/change-object-modal.vue
@@ -2,24 +2,43 @@
    <div>
       <v-dialog v-on:keydown.esc="hide()" v-on:keydown.enter="submit()" :value="visible" persistent max-width="310">
          <v-card>
-            <v-card-title class="headline">Change script {{$options.filters.object_label(type).toLowerCase()}}</v-card-title>
+            <v-card-title class="headline">Change script {{$options.filters.object_label(type).toLowerCase()}}
+            </v-card-title>
             <v-card-text>
-               <v-text-field autofocus v-if="visible" v-model="object" :label="`Change ${$options.filters.object_label(type).toLowerCase()}`" required></v-text-field>
+               <v-text-field autofocus v-if="visible" v-model="object"
+                             :label="`Change ${$options.filters.object_label(type).toLowerCase()}`"
+                             required></v-text-field>
+            </v-card-text>
+            <v-card-text class="pt-0" v-if="type === 'BUS_EVENT'">
+               <a class="pa-1" href="#" @click="set_object('.+')">All topics</a>
+               <a class="pa-1" href="#" @click="set_object('.+test.+')">All topics with 'test'</a>
+            </v-card-text>
+            <v-card-text v-if="type === 'TIMER_EVENT'">
+               <a class="pa-1" href="#" @click="set_object('1')">Every second</a>
+               <a class="pa-1" href="#" @click="set_object('10')">Every 10 seconds</a>
+            </v-card-text>
+            <v-card-text v-if="type === 'SCHEDULE_EVENT'">
+               <a class="pa-1" href="#" @click="set_object('0 0 * * * *')">Every hour</a>
+               <a class="pa-1" href="#" @click="set_object('0 0 30 * * * *')">Every 30 minutes</a>
+            </v-card-text>
+            <v-card-text v-if="type === 'WEB_EVENT'">
+               <a class="pa-1" href="#" @click="set_object()">By the script name</a>
             </v-card-text>
             <v-card-text v-if="type === 'SCHEDULE_EVENT'" class="pt-0">
                <code class="cron-code font-weight-thin">
-   * * * * * *
-   │ │ │ │ │ └── Day of the Week
-   │ │ │ │ │     (SUN-SAT or 0-6 or "*")
-   │ │ │ │ └──── Month of the Year
-   │ │ │ │       (JAN-DEC or 1-12 or "*")
-   │ │ │ └────── Day of the Month
-   │ │ │         (0-30 or "*")
-   │ │ └──────── Hour (0-24 or "*")
-   │ └────────── Minute (0-59 or "*")
-   └──────────── Second (0-59)
+                  * * * * * *
+                  │ │ │ │ │ └── Day of the Week
+                  │ │ │ │ │ (SUN-SAT or 0-6 or "*")
+                  │ │ │ │ └──── Month of the Year
+                  │ │ │ │ (JAN-DEC or 1-12 or "*")
+                  │ │ │ └────── Day of the Month
+                  │ │ │ (0-30 or "*")
+                  │ │ └──────── Hour (0-24 or "*")
+                  │ └────────── Minute (0-59 or "*")
+                  └──────────── Second (0-59)
                </code><br>
-               <span>Schedule in a unix-like <a href="https://en.wikipedia.org/wiki/Cron" target="_blank">cron format</a> with seconds.</span>
+               <span>Schedule in a unix-like <a href="https://en.wikipedia.org/wiki/Cron"
+                                                target="_blank">cron format</a> with seconds.</span>
             </v-card-text>
             <v-card-actions>
                <v-btn color="error" flat @click="hide()">Cancel</v-btn>
@@ -59,6 +78,13 @@ export default {
       this.object = "";
       this.uuid = "";
       this.type = "";
+    },
+    set_object(value){
+        if (this.type === "WEB_EVENT") {
+            this.object = this.$parent.name;
+        }else {
+            this.object = value;
+        }
     },
     submit() {
       Vue.axios
