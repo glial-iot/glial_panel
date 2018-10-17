b045a99d8b44d08f16e4db776101d68a5b0007ea
Nikolay Matyukhin <matuhin86@gmail.com>
2018-10-17 18:51:20 +0300

    Some refactoring and bugfixes (modals, load same script twice onchange event triggering)

--- src/components/modals/change-object-modal.vue
+++ src/components/modals/change-object-modal.vue
@@ -9,10 +9,7 @@
                              :label="`Change ${$options.filters.object_label(type).toLowerCase()}`"
                              required></v-text-field>
             </v-card-text>
-            <v-card-text class="pt-0" v-if="type === 'BUS_EVENT'">
-               <a title="Set mask to select all topics" class="pt-1 pr-1 pb-1" href="#" @click="set_object('.+')">All topics</a>
-               <a title="Set mask to filter only topics containing 'test'" class="pa-1" href="#" @click="set_object('.+test.+')">All topics with 'test'</a>
-            </v-card-text>
+            <bus-event-shortcuts class="pt-0" v-if="type === 'BUS_EVENT'" v-on:set_object="object = $event; get_mask_match()"></bus-event-shortcuts>
             <v-card-text class="matching_topics_list pt-0" v-if="type === 'BUS_EVENT'">
                <h4 class="matching_topics_header">Matching topics: </h4>
                <div class="matching_topic" v-for="item in mask_topics">
@@ -20,37 +17,14 @@
                </div>
                <div class="matching_topic" v-if="Object.keys(mask_topics).length === 0" >No match</div>
             </v-card-text>
-            <v-card-text class="pt-0" v-if="type === 'WEB_EVENT'">
-               <a title="Set endpoint name equal to script name" class="pt-1 pr-1 pb-1" href="#" @click="set_object()">By the script name</a>
-            </v-card-text>
+            <timer-event-shortcuts class="pt-0" v-if="type === 'TIMER_EVENT'" v-on:set_object="object = $event"></timer-event-shortcuts>
+            <schedule-event-shortcuts class="pt-0" v-if="type === 'SHEDULE_EVENT'" v-on:set_object="object = $event"></schedule-event-shortcuts>
+            <schedule-event-crontab-description class="pt-0" v-if="type === 'SHEDULE_EVENT'"></schedule-event-crontab-description>
+            <web-event-shortcuts v-on:set_by_script_name="object = $parent.name" v-if="type === 'WEB_EVENT'"></web-event-shortcuts>
             <v-card-text v-if="type === 'WEB_EVENT'">
                <span>Full script URL:
                   <a :href="$store.getters.server_url+'/we/'+object" target="_blank">{{$store.getters.server_url}}/we/{{object || "endpoint"}}</a>
                </span>
-            </v-card-text>
-            <v-card-text class="pt-0" v-if="type === 'TIMER_EVENT'">
-               <a title="Set interval to 1 second" class="pt-1 pr-1 pb-1" href="#" @click="set_object('1')">Every second</a>
-               <a title="Set interval to 10 seconds" class="pa-1" href="#" @click="set_object('10')">Every 10 seconds</a>
-            </v-card-text>
-            <v-card-text class="pt-0" v-if="type === 'SHEDULE_EVENT'">
-               <a title="Set schedule to run script every hour" class="pt-1 pr-1 pb-1" href="#" @click="set_object('0 0 * * * *')">Every hour</a>
-               <a title="Set schedule to run script every 30 minutes" class="pa-1" href="#" @click="set_object('0 0 30 * * * *')">Every 30 minutes</a>
-            </v-card-text>
-            <v-card-text class="pt-0" v-if="type === 'SHEDULE_EVENT'">
-               <code class="cron-code font-weight-thin">
-                  * * * * * *
-                  │ │ │ │ │ └── Day of the Week
-                  │ │ │ │ │ (SUN-SAT or 0-6 or "*")
-                  │ │ │ │ └──── Month of the Year
-                  │ │ │ │ (JAN-DEC or 1-12 or "*")
-                  │ │ │ └────── Day of the Month
-                  │ │ │ (0-30 or "*")
-                  │ │ └──────── Hour (0-24 or "*")
-                  │ └────────── Minute (0-59 or "*")
-                  └──────────── Second (0-59)
-               </code><br>
-               <span>Schedule in a unix-like <a href="https://en.wikipedia.org/wiki/Cron"
-                                                target="_blank">cron format</a> with seconds.</span>
             </v-card-text>
             <v-card-actions>
                <v-btn color="error" flat @click="hide()">Cancel</v-btn>
@@ -69,8 +43,21 @@ import Axios from "axios";
 import VueAxios from "vue-axios";
 Vue.use(VueAxios, Axios);
 import snackbar from "../snackbar.vue";
+import busEventShortcuts from "../blocks/shortcuts/bus_event_shortcuts.vue";
+import timerEventShortcuts from "../blocks/shortcuts/timer_event_shortcuts.vue";
+import scheduleEventShortcuts from "../blocks/shortcuts/schedule_event_shortcuts.vue";
+import webEventShortcuts from "../blocks/shortcuts/web_event_shortcuts.vue";
+import scheduleEventCrontabDescription from "../blocks/misc/schedule-event-crontab-description.vue";
 
 export default {
+  components: {
+        snackbar,
+        busEventShortcuts,
+        timerEventShortcuts,
+        scheduleEventShortcuts,
+        scheduleEventCrontabDescription,
+        webEventShortcuts
+    },
   props: ["updateObject"],
   data: () => ({
     object: "",
@@ -93,14 +80,6 @@ export default {
       this.uuid = "";
       this.type = "";
       this.mask_topics = [];
-    },
-    set_object(value){
-        if (this.type === "WEB_EVENT") {
-            this.object = this.$parent.name;
-        }else {
-            this.object = value;
-        }
-        this.get_mask_match();
     },
     get_mask_match() {
         console.log("hit");
--- src/components/modals/load_script_modal.vue
+++ src/components/modals/load_script_modal.vue
@@ -6,7 +6,7 @@
         </v-btn>
 
         <form>
-            <input @change="handle_file_change" id="fileInput" type="file" style="display:none;"/>
+            <input ref="fileInputImport" @change="handle_file_change" id="fileInput" type="file" style="display:none;"/>
         </form>
 
         <v-dialog v-on:keydown.esc="close()" v-on:keydown.enter="create_loaded_script()" v-model="visible"
@@ -25,45 +25,22 @@
                     <v-text-field @keyup="get_mask_match()" :label="$options.filters.object_label(type)"
                                   v-model="object"></v-text-field>
                 </v-card-text>
-                <v-card-text class="pt-0" v-if="type === 'BUS_EVENT'">
-                    <a title="Set mask to select all topics" class="pt-1 pr-1 pb-1" href="#" @click="set_object('.+')">All topics</a>
-                    <a title="Set mask to filter only topics containing 'test'" class="pa-1" href="#" @click="set_object('.+test.+')">All topics with 'test'</a>
-                </v-card-text>
-                <v-card-text v-if="type === 'BUS_EVENT'">
+                <bus-event-shortcuts class="pt-0" v-if="type === 'BUS_EVENT'" v-on:set_object="object = $event; get_mask_match()"></bus-event-shortcuts>
+                <v-card-text class="matching_topics_list pt-0" v-if="type === 'BUS_EVENT'">
                     <h4 class="matching_topics_header">Matching topics: </h4>
                     <div class="matching_topic" v-for="item in mask_topics">
                         {{item.topic}}
                     </div>
-                    <div class="matching_topic" v-if="Object.keys(mask_topics).length === 0">No match</div>
+                    <div class="matching_topic" v-if="Object.keys(mask_topics).length === 0" >No match</div>
                 </v-card-text>
-                <v-card-text class="pt-0" v-if="type === 'WEB_EVENT'">
-                    <a title="Set endpoint name equal to script name" class="pt-1 pr-1 pb-1" href="#" @click="set_object()">By the script name</a>
-                </v-card-text>
+                <timer-event-shortcuts class="pt-0" v-if="type === 'TIMER_EVENT'" v-on:set_object="object = $event"></timer-event-shortcuts>
+                <schedule-event-shortcuts class="pt-0" v-if="type === 'SHEDULE_EVENT'" v-on:set_object="object = $event"></schedule-event-shortcuts>
+                <schedule-event-crontab-description class="pt-0" v-if="type === 'SHEDULE_EVENT'"></schedule-event-crontab-description>
+                <web-event-shortcuts v-on:set_by_script_name="object = name" v-if="type === 'WEB_EVENT'"></web-event-shortcuts>
                 <v-card-text v-if="type === 'WEB_EVENT'">
                <span>Full script URL:
                   <a :href="$store.getters.server_url+'/we/'+object" target="_blank">{{$store.getters.server_url}}/we/{{object || "endpoint"}}</a>
                </span>
-                </v-card-text>
-                <v-card-text class="pt-0" v-if="type === 'TIMER_EVENT'">
-                    <a title="Set interval to 1 second" class="pt-1 pr-1 pb-1" href="#" @click="set_object('1')">Every second</a>
-                    <a title="Set interval to 10 seconds" class="pa-1" href="#" @click="set_object('10')">Every 10 seconds</a>
-                </v-card-text>
-                <v-card-text class="pt-0" v-if="type === 'SHEDULE_EVENT'">
-                    <a title="Set schedule to run script every hour" class="pt-1 pr-1 pb-1" href="#" @click="set_object('0 0 * * * *')">Every hour</a>
-                    <a title="Set schedule to run script every 30 minutes" class="pa-1" href="#" @click="set_object('0 0 30 * * * *')">Every 30 minutes</a>
-                </v-card-text>
-                <v-card-text v-if="type === 'SHEDULE_EVENT'" class="pt-0">
-                    <code class="cron-code font-weight-thin">
-                        * * * * * *
-                        │ │ │ │ │ │
-                        │ │ │ │ │ └── Day of the Week (SUN-SAT or 0-6 or "*")
-                        │ │ │ │ └──── Month of the Year (JAN-DEC or 1-12 or "*")
-                        │ │ │ └────── Day of the Month (0-30 or "*")
-                        │ │ └──────── Hour (0-24 or "*")
-                        │ └────────── Minute (0-59 or "*")
-                        └──────────── Second (0-59)
-                    </code><br>
-                    <span>Schedule in a unix-like <a href="https://en.wikipedia.org/wiki/Cron" target="_blank">cron format</a> with seconds.</span>
                 </v-card-text>
                 <v-divider></v-divider>
                 <v-card-actions>
@@ -79,34 +56,27 @@
         <snackbar ref="snackbar"></snackbar>
     </div>
 </template>
-<style>
-    .matching_topics_header {
-        color: rgba(0, 0, 0, .54);
-        font-weight: normal;
-    }
-
-    .matching_topic {
-        font-size: 12px;
-    }
-
-    .cron-code {
-        background: none;
-        color: inherit;
-        box-shadow: none;
-        line-height: 120%;
-    }
-</style>
 <script>
     import Vue from "vue";
     import Axios from "axios";
     import VueAxios from "vue-axios";
     import snackbar from "../snackbar.vue";
+    import busEventShortcuts from "../blocks/shortcuts/bus_event_shortcuts.vue";
+    import timerEventShortcuts from "../blocks/shortcuts/timer_event_shortcuts.vue";
+    import scheduleEventShortcuts from "../blocks/shortcuts/schedule_event_shortcuts.vue";
+    import webEventShortcuts from "../blocks/shortcuts/web_event_shortcuts.vue";
+    import scheduleEventCrontabDescription from "../blocks/misc/schedule-event-crontab-description.vue";
 
     Vue.use(VueAxios, Axios);
 
     export default {
         components: {
-            snackbar
+            snackbar,
+            busEventShortcuts,
+            timerEventShortcuts,
+            scheduleEventShortcuts,
+            scheduleEventCrontabDescription,
+            webEventShortcuts
         },
         data() {
             return {
@@ -138,8 +108,10 @@
                     }
                 };
                 fr.readAsText(event.target.files[0]);
+                this.$refs.fileInputImport.value = ""
             },
             initialize_data(data) {
+                console.log("initializing");
                 this.name = data.name;
                 this.object = data.object;
                 this.body = data.body;
@@ -152,14 +124,6 @@
                 this.visible = false;
                 this.body = "";
                 this.mask_topics = [];
-            },
-            set_object(value) {
-                if (this.type === "WEB_EVENT") {
-                    this.object = this.name;
-                } else {
-                    this.object = value;
-                }
-                this.get_mask_match();
             },
             get_mask_match() {
                 if (this.object !== "" && this.type === "BUS_EVENT") {
