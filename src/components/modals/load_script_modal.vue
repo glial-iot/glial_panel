e1ebe185e3fa99c68497c1d6e515496ba8c1bfd7
Nikolay Matyukhin <matuhin86@gmail.com>
2018-11-06 22:45:07 +0300

    Removed modal dialog after script file upload. Now it uploads without any questions and is saved with original name and object.

--- src/components/modals/load_script_modal.vue
+++ src/components/modals/load_script_modal.vue
@@ -9,50 +9,6 @@
             <input ref="fileInputImport" @change="handle_file_change" id="fileInput" type="file" style="display:none;"/>
         </form>
 
-        <v-dialog v-on:keydown.esc="close()" v-on:keydown.enter="create_loaded_script()" v-model="visible"
-                  max-width="500px">
-            <v-card>
-                <v-card-title>
-                    <div class="title text-xs-center">Load {{$options.filters.type2string(type).toLowerCase()}} as
-                        "{{name}}"
-                    </div>
-                </v-card-title>
-                <v-divider></v-divider>
-                <v-card-text class="py-0">
-                    <v-text-field autofocus v-if="visible" label="Name" v-model="name"></v-text-field>
-                </v-card-text>
-                <v-card-text v-if="type != 'DRIVER'" class="py-0">
-                    <v-text-field @keyup="get_mask_match()" :label="$options.filters.object_label(type)"
-                                  v-model="object"></v-text-field>
-                </v-card-text>
-                <bus-event-shortcuts class="pt-0" v-if="type === 'BUS_EVENT'" v-on:set_object="object = $event; get_mask_match()"></bus-event-shortcuts>
-                <v-card-text class="matching_topics_list pt-0" v-if="type === 'BUS_EVENT'">
-                    <h4 class="matching_topics_header">Matching topics: </h4>
-                    <div class="matching_topic" v-for="item in mask_topics">
-                        {{item.topic}}
-                    </div>
-                    <div class="matching_topic" v-if="Object.keys(mask_topics).length === 0" >No match</div>
-                </v-card-text>
-                <timer-event-shortcuts class="pt-0" v-if="type === 'TIMER_EVENT'" v-on:set_object="object = $event"></timer-event-shortcuts>
-                <schedule-event-shortcuts class="pt-0" v-if="type === 'SHEDULE_EVENT'" v-on:set_object="object = $event"></schedule-event-shortcuts>
-                <schedule-event-crontab-description class="pt-0" v-if="type === 'SHEDULE_EVENT'"></schedule-event-crontab-description>
-                <web-event-shortcuts v-on:set_by_script_name="object = name" v-if="type === 'WEB_EVENT'"></web-event-shortcuts>
-                <v-card-text v-if="type === 'WEB_EVENT'">
-               <span>Full script URL:
-                  <a :href="$store.getters.server_url+'/we/'+object" target="_blank">{{$store.getters.server_url}}/we/{{object || "endpoint"}}</a>
-               </span>
-                </v-card-text>
-                <v-divider></v-divider>
-                <v-card-actions>
-                    <v-btn color="error" flat @click.stop="close">Close</v-btn>
-                    <v-spacer></v-spacer>
-                    <v-btn color="green darken-1" right flat @click="create_loaded_script()">
-                        <v-icon left small>fa-file</v-icon>
-                        Create
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
         <snackbar ref="snackbar"></snackbar>
     </div>
 </template>
@@ -61,31 +17,12 @@
     import Axios from "axios";
     import VueAxios from "vue-axios";
     import snackbar from "../snackbar.vue";
-    import busEventShortcuts from "../blocks/shortcuts/bus_event_shortcuts.vue";
-    import timerEventShortcuts from "../blocks/shortcuts/timer_event_shortcuts.vue";
-    import scheduleEventShortcuts from "../blocks/shortcuts/schedule_event_shortcuts.vue";
-    import webEventShortcuts from "../blocks/shortcuts/web_event_shortcuts.vue";
-    import scheduleEventCrontabDescription from "../blocks/misc/schedule-event-crontab-description.vue";
 
     Vue.use(VueAxios, Axios);
 
     export default {
         components: {
             snackbar,
-            busEventShortcuts,
-            timerEventShortcuts,
-            scheduleEventShortcuts,
-            scheduleEventCrontabDescription,
-            webEventShortcuts
-        },
-        data() {
-            return {
-                visible: false,
-                name: "",
-                object: "",
-                body: "",
-                mask_topics: []
-            };
         },
         props: ["type"],
         methods: {
@@ -99,7 +36,7 @@
                 fr.onload = (event) => {
                     result = JSON.parse(event.target.result);
                     if (result.type === this.type) {
-                        this.initialize_data(result);
+                        this.create_loaded_script(result.name, result.object, result.body)
                     }
                     else {
                         this.$refs.snackbar.update(
@@ -110,65 +47,13 @@
                 fr.readAsText(event.target.files[0]);
                 this.$refs.fileInputImport.value = ""
             },
-            initialize_data(data) {
-                console.log("initializing");
-                this.name = data.name;
-                this.object = data.object;
-                this.body = data.body;
-                this.get_mask_match();
-                this.visible = true;
-            },
-            close() {
-                this.name = "";
-                this.object = "";
-                this.visible = false;
-                this.body = "";
-                this.mask_topics = [];
-            },
-            get_mask_match() {
-                if (this.object !== "" && this.type === "BUS_EVENT") {
-                    Vue.axios
-                        .get(
-                            "http://localhost:8080/system_bus",
-                            {
-                                params: {
-                                    action: "get_bus",
-                                    limit: 10,
-                                    mask: btoa(this.object)
-                                }
-                            }
-                        )
-                        .then(response => {
-                            if (response.data.length > 0) {
-                                this.mask_topics = response.data;
-                            }
-                            else {
-                                this.mask_topics = [];
-                            }
-
-                        })
-                        .catch(error => {
-                            this.mask_topics = [];
-                            console.log(error);
-                        });
-                }
-                else {
-                    this.mask_topics = [];
-                }
-            },
-            create_loaded_script() {
-                if (this.type !== "DRIVER" && this.object === "") {
-                    this.$refs.snackbar.update(
-                        `Field "${this.$options.filters.object_label(this.type)}" is required`
-                    );
-                    return;
-                }
+            create_loaded_script(name, object, body) {
                 let params = {
                     action: "create",
-                    name: this.name
+                    name: name
                 };
                 if (this.type !== "DRIVER") {
-                    params.object = this.object;
+                    params.object = object;
                 }
                 Vue.axios.get(
                     this.$store.getters.server_url +
@@ -176,15 +61,15 @@
                     {params}
                 )
                     .then(response => {
-                        this.update_script_body(response);
+                        this.update_script_body(response, body);
                     })
                     .catch(error => {
                         console.log(error);
                         this.$emit("load_script_error");
                     });
             },
-            update_script_body(script) {
-                let data = new Blob([this.body], {
+            update_script_body(script, body) {
+                let data = new Blob([body], {
                     type: "text/plain"
                 });
                 let reader = new FileReader();
@@ -200,7 +85,6 @@
                         .then(response => {
                             this.$emit("script_loaded", this.item);
                             this.$refs.snackbar.update("Script loaded and saved", "success", 3000);
-                            this.close();
                         })
                         .catch(error => {
                             console.log(error);
